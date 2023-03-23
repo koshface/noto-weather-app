b067ec86d9295acaac7c6004406a7c590107a006
koshface <koshface@gmail.com>
2023-03-23 14:54:12 -0500

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Emily Noto and is {""}
+        <a href="https://github.com/koshface/noto-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
