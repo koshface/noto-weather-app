220eccc0eaea3a70d4a09e94f06ea3744fe77070
koshface <koshface@gmail.com>
2023-03-27 08:17:03 -0500

    Set up HTML

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Emily Noto and is {""}
-        <a href="https://github.com/koshface/noto-weather-app" target="_blank">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Emily Noto and is {""}
+          <a
+            href="https://github.com/koshface/noto-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
